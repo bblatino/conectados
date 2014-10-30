cef8540b2232927da19ed0679ce5ed15d2fab8c0
Carlos Mantilla <ceo@aliongroo.com>
2014-10-30 06:51:35 +0200

    feat: login component.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('conectadosApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $window) {
+    // Methods.
+    $scope.logout = function() {
+      $window.gapi.auth.signOut();
+    };
 
+
+    // Observers.
+    $scope.$on('event:google-plus-signin-success', function(event, args) {
+      console.log('event:google-plus-signin-success', args);
+
+      if (args.status.google_logged_in) {
+        $scope.logged = true;
+        $scope.$digest();
+      }
+    });
+
+    $scope.$on('event:google-plus-signin-failure', function() {
+      console.log('event:google-plus-signin-failure');
+
+      $scope.logged = false;
+    });
   });
