ccb8a1fc4a47fe3a7c79d3fad63d652358dd8480
Carlos Mantilla <ceo@aliongroo.com>
2014-12-04 08:22:53 +0200

    fix(gruopojoven): url tt4 configuracion

--- app/scripts/services/grupojoven-soporte.js
+++ app/scripts/services/grupojoven-soporte.js
@@ -34,7 +34,7 @@ angular.module('conectadosApp')
           description: 'Para configurar automaticamente el team talk 4, descargar y hacer hacer doble click.',
           language: 'es',
           img: 'http://google.github.io/material-design-icons/file/svg/ic_file_download_24px.svg',
-          url: '/files/tt4_grupo_joven.tt',
+          url: '/conectados/files/tt4_grupo_joven.tt',
           type: 'file'
         },
         {
@@ -42,7 +42,7 @@ angular.module('conectadosApp')
           description: 'Para configurar automaticamente o team talk 4, baxiare fazer com que você dê um duplo clique.',
           language: 'pt',
           img: 'http://google.github.io/material-design-icons/file/svg/ic_file_download_24px.svg',
-          url: '/files/tt4_grupo_joven.tt',
+          url: '/conectados/files/tt4_grupo_joven.tt',
           type: 'file'
         }
 
