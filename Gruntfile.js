1658ad6bea27033377c6c0bf9a8d347876c4bdb7
Carlos Mantilla <ceo@aliongroo.com>
2014-12-26 07:58:06 +0200

    Merge 16 into master

--- Gruntfile.js
+++ Gruntfile.js
@@ -310,7 +310,8 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
+            'fonts/*',
+            'files/*'
           ]
         }, {
           expand: true,
