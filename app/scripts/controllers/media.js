7d8c153e7847539facc4bf03f93afa9f419fa005
Carlos Mantilla <ceo@aliongroo.com>
2015-01-23 19:52:06 +0200

    WIP: Agregando el markup de las siguientes transmisiones a la página media.html. Y creando enlaces en el menu.

--- app/scripts/controllers/media.js
+++ app/scripts/controllers/media.js
@@ -8,6 +8,15 @@
  * Controller of the conectadosApp
  */
 angular.module('conectadosApp')
-  .controller('MediaCtrl', function ($scope) {
-    console.log('MediaCtrl');
+  .controller('MediaCtrl', function ($scope, $anchorScroll, $location) {
+    /**
+     * Go to the section in the content
+     *
+     * @param section
+     *  The name of the section.
+     */
+    $scope.goToSection = function(section) {
+      $location.hash(section);
+      $anchorScroll();
+    };
   });
