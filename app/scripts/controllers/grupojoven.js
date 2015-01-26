b922765b178063e9ef545a67c6871dae33806705
Carlos Mantilla <ceo@aliongroo.com>
2015-01-26 19:15:26 +0200

    feat: login the user with google account.

--- app/scripts/controllers/grupojoven.js
+++ app/scripts/controllers/grupojoven.js
@@ -1,26 +1,20 @@
 'use strict';
 
 angular.module('conectadosApp')
-  .controller('GrupoJovenCtrl', function (OAuth) {
-    //Using popup
-    OAuth.popup('google')
-      .done(function(result) {
-        //use result.access_token in your API request
-        //or use result.get|post|put|del|patch|me methods (see below)
-        result.me()
-          .done(function (response) {
-            //this will display "John Doe" in the console
-            console.log(response.name);
-          })
-          .fail(function (err) {
-            //handle error with err
-            console.log('error: ', err);
-          });
+  .controller('GrupoJovenCtrl', function (OAuth, User) {
 
-        console.log('autehnticado: ', result.access_token);
-      })
-      .fail(function (err) {
-        //handle error with err
-        console.log('error: ', err);
-      });
+    /**
+     * Login the user using OAuth service.
+     *
+     */
+    this.login = function() {
+      OAuth.login();
+    };
+
+
+    this.signup = function(user) {
+      User.signup(user);
+    };
+
+
   });
--- app/scripts/services/oauthio.js
+++ app/scripts/services/oauthio.js
@@ -5,7 +5,36 @@
 
 angular.module('conectadosApp')
   .service('OAuth', function OAuthService($window) {
+    var OAuth = this;
+
+    // Extend the library.
     angular.extend(this, $window.OAuth);
 
+    /**
+     *
+     * @param provider
+     */
+    this.login = function() {
+      //Using popup
+      OAuth.popup('google')
+        .done(function(result) {
+          //use result.access_token in your API request
+          //or use result.get|post|put|del|patch|me methods (see below)
+          result.me()
+            .done(function (response) {
+              //this will display "John Doe" in the console
+              console.log(response.name);
+            })
+            .fail(function (err) {
+              //handle error with err
+              console.log('error: ', err);
+            });
 
+          console.log('autehnticado: ', result.access_token);
+        })
+        .fail(function (err) {
+          //handle error with err
+          console.log('error: ', err);
+        });
+    };
   });
