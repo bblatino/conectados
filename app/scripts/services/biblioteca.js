d69c43418a824c5b29e61ac5ef68568c7f62ba6b
abelardovela <bbabelardo@gmail.com>
2015-02-12 21:57:19 -0600

    agregando lista de libros en biblioteca

--- app/scripts/services/biblioteca.js
+++ app/scripts/services/biblioteca.js
@@ -21,29 +21,175 @@ angular.module('conectadosApp')
           url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
         },
         {
-          name: 'El punto en el corazón.',
-          description: 'Origen del placer para mi alma.',
+          name: 'Torre de Babel-Último piso.',
+          description: 'Israel y el futuro de la humanidad.',
           tags: 'cabalá',
           img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
           url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
         },
         {
-          name: 'El punto en el corazón.',
-          description: 'Origen del placer para mi alma.',
+          name: 'Cabalá para principiantes',
+          description: 'Una introducción a la sabiduría oculta',
           tags: 'cabalá',
           img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
           url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
         },
         {
-          name: 'El punto en el corazón.',
-          description: 'Origen del placer para mi alma.',
+          name: 'La voz de la Cabalá',
+          description: 'Para quien busca expandir su visión interna',
           tags: 'cabalá',
           img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
           url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
         },
         {
-          name: 'El punto en el corazón.',
-          description: 'Origen del placer para mi alma.',
+          name: 'El poder de la Cabalá',
+          description: 'Un manual de instrucciones para la vida',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: 'Tu propósito en la vida',
+          description: 'La sabiduría de la Cabalá explica cómo organizar la vida sobre la tierra correctamente',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: 'Alcanzando los mundos superiores',
+          description: 'Guía para el descubrimiento espíritual',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: 'Cabalá para aprendices',
+          description: 'Principios básicos para una vida plena',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: 'Cabalá para no iniciados',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: 'Rescate de la crisis mundial',
+          description: 'Una guía práctica para emerger fortalecidos',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: 'Integrándonos según las leyes de la naturaleza',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: 'La guía para el mundo nuevo',
+          description: 'Por qué la garantía mutua es la clave para nuestra recuperación de la crisis global',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: 'Aprender jugando',
+          description: 'Guía de juegos constructivos y comunicativos',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },{
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
+          tags: 'cabalá',
+          img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
+          url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
+        },
+        {
+          name: '',
+          description: '',
           tags: 'cabalá',
           img: 'http://google.github.io/material-design-icons/action/svg/ic_description_48px.svg',
           url: 'https://drive.google.com/file/d/0B9Yta2OKAh8xS2I0Ny16SEtUams/view?usp=sharing'
