01af68963c606351cecd40a66cb0f8d53616cdb8
Carlos Mantilla <ceo@aliongroo.com>
2015-04-13 22:26:47 +0300

    WIP: Implementing register user.

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,8 +1,28 @@
 'use strict';
 
 angular.module('conectadosApp')
-  .controller('RegisterCtrl', function () {
-    this.register = function() {
-      console.log('registring');
+  .controller('RegisterCtrl', function (authService) {
+    this.register = function(form) {
+      // Validate if password ok.
+      if (form.password.$viewValue !== form.passwordrepeated.$viewValue) {
+        console.log('Password have to be that same');
+        return;
+      }
+
+      // Ceate user object.
+      var userObj = {
+        email: form.email.$viewValue,
+        password: form.password.$viewValue
+      };
+
+      authService.createUser(userObj, function userCreated(user) {
+        // Clear wrote data before do the login.
+        form.email.$viewValue = '';
+        form.password.$viewValue = '';
+        form.passwordrepeated.$viewValue = '';
+
+        console.log(user);
+      });
+
     };
   });
