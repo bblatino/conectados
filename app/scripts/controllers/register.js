7a3a04ecc61d1ce56f13ebb8a2f79be96b3ccf7e
Carlos Mantilla <ceo@aliongroo.com>
2015-06-06 13:50:31 +0300

    feat: add toast to handle message from the form.

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,11 +1,14 @@
 'use strict';
 
 angular.module('conectadosApp')
-  .controller('RegisterCtrl', function (authService, $mdToast) {
+  .controller('RegisterCtrl', function (authService, $mdToast, $location) {
     this.register = function(form) {
       // Validate if password ok.
       if (form.password.$viewValue !== form.passwordrepeated.$viewValue) {
-        console.log('Password have to be that same');
+        // Error message.
+        $mdToast.show($mdToast.simple()
+          .content('Las contraseñas tiene que ser iguales')
+          .position('top right'));
         return;
       }
 
@@ -16,14 +19,20 @@ angular.module('conectadosApp')
       };
 
       authService.createUser(userObj, function userCreated(user) {
-        $mdToast.simple();
+
+        debugger;
+        $mdToast.show($mdToast.simple()
+          .content('Usuario Registrado')
+          .position('top right'));
+
 
         // Clear wrote data before do the login.
         form.email.$viewValue = '';
         form.password.$viewValue = '';
         form.passwordrepeated.$viewValue = '';
 
-        console.log(user);
+        // Back to the principal system
+        //$location.path('#/grupojoven');
       });
 
     };
